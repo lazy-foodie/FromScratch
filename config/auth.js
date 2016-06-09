111c6057e1b373d52d1bc1a6909cac41ed5e78b3
Bich Diep <diepb@seattleu.edu>
2016-06-08 18:21:05 -0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1581387208825676', 
         'clientSecret'  : 'b7fc313831476391b8a2c250045bd236', 
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'http://lazyfoodie.azurewebsites.net/auth/facebook/callback',
 	    'profileFields': ['id', 'name', 'displayName', 'email']
 
 	}
